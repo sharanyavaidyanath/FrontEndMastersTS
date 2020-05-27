f079836344c63583a2c5e1bcc227a982935bf128
Sharanya <sharuv89@gmail.com>
2020-05-27 16:46:17 +0530

    Adding types

--- challenges/address-book/src/index.ts
+++ challenges/address-book/src/index.ts
@@ -1,13 +1,14 @@
+interface Person  {firstName?: string; middleName?: string; lastName?: string; salutation?: string; email?: string;  phones: { [k: string]: number;}; addresses: {[k: string]: {houseNumber: number; street: string; city: string; state: string; postalCode: number; counrty: string }}
 export class AddressBook {
-  contacts = [];
+  contacts: Person[]= [];
 
-  addContact(contact) {
+  addContact(contact: Person) {
     this.contacts.push(contact);
   }
 
-  findContactByName(filter) {
+  findContactByName(filter: {firstName?: string, lastName?: string}) {
     return this.contacts.filter(c => {
-      if (
+      if (   
         typeof filter.firstName !== "undefined" &&
         c.firstName !== filter.firstName
       ) {
@@ -24,7 +25,7 @@ export class AddressBook {
   }
 }
 
-export function formatDate(date) {
+export function formatDate(date: Date) {
   return (
     date
       .toISOString()
@@ -33,13 +34,13 @@ export function formatDate(date) {
   );
 }
 
-function getFullName(contact) {
+function getFullName(contact : Person) {
   return [contact.firstName, contact.middleName, contact.lastName]
     .filter(Boolean)
     .join(" ");
 }
 
-export function getVcardText(contact, date = new Date()) {
+export function getVcardText(contact: Person, date = new Date()) {
   const parts = [
     "BEGIN:VCARD",
     "VERSION:2.1",
